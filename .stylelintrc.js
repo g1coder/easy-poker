056fe405aa070d06a0ce0f201149f4b417ace318
vitaly.lukyanenko <lukyanenko.v@promail366.com>
2025-10-26 11:30:22 +0700

    init commit

---
+++ .stylelintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: "stylelint-config-standard-scss",
+    customSyntax: "postcss-scss",
+    rules: {
+        "declaration-block-no-redundant-longhand-properties": null,
+        "media-feature-range-notation": null,
+        "custom-property-pattern": null,
+        "selector-class-pattern": null,
+        "selector-id-pattern": null,
+        "scss/dollar-variable-pattern": null,
+    },
+};
---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+    endOfLine: 'lf',
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    printWidth: 120,
+};
+
+export default config;
